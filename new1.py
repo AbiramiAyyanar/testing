1689bb40d6e2e4c793cd9d0d2f168f51c6f071af
AbiramiAyyanar <abiayyanar01@gmail.com>
2025-06-10 14:35:58 +0530

    Update new1.py

--- new1.py
+++ new1.py
@@ -1,28 +1,3 @@
-# import mysql.connector
-
-# # Connect to MySQL
-# conn = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password="vglug@123",
-#     database="Sample"
-# )
-
-# cursor = conn.cursor()
-
-# # Retrieve data
-# cursor.execute("SELECT * FROM SAMPLE")  # Replace 'users' with your table name
-# rows = cursor.fetchall()
-
-# # Display results
-# for row in rows:
-#     print(row)
-
-# # Close connection
-# cursor.close()
-# conn.close()
-
-
 n=int(input("enter a number:"))
 if n%2==0:
     print("even")
